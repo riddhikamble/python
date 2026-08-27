562f92a9803c3d414b4089aab0c3df38f5ccb499
riddhikamble <riddhishrikrishnakamble@gmail.com>
2026-08-27 16:10:11 +0530

    Add files via upload

---
+++ lab3.1.py
@@ -0,0 +1,8 @@
+age=int(input("Enter Your Age :"))
+
+income=float(input("Enter Your Income :"))
+
+if age < 25 and income  <300000:
+    print("Congratulations! You qualify for the specialized education scholarship")
+else:
+    print("Sorry! You do not qualify for the scholarship ")
